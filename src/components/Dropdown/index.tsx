54150bbe5c94cc0a46923b9118ae3a4c9cef8aad
Gyujae <wjdrbwo1206@naver.com>
2022-05-05 18:21:18 +0900

    dropdown classname refatoring

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -29,23 +29,15 @@ function Dropdown() {
       </div>
       {showSearch && (
         <div className={styles.searchContainer}>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-6 w-6 absolute top-5 left-2 text-gray-400'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-            strokeWidth={2}
-          >
+          <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
             <path strokeLinecap='round' strokeLinejoin='round' d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' />
           </svg>
           <input
-            className='bg-gray-50 border-[2px] w-full h-16 rounded-t-md outline-none pr-2 pl-10 text-xl text-gray-500'
             value={searchValue}
             placeholder='Search Symbol'
             onChange={(event) => setSearchValue(event.target.value)}
           />
-          <ul className='bg-gray-50 border-[2px] border-t-0 space-y-2 rounded-b-md'>
+          <ul>
             {data
               .filter(
                 (symbolName) =>
@@ -53,12 +45,7 @@ function Dropdown() {
                   symbolName.toLocaleUpperCase().includes(searchValue)
               )
               .map((symbolName) => (
-                <button
-                  key={symbolName}
-                  className='pl-10 text-xl py-4 hover:bg-gray-100 cursor-pointer text-gray-500 font-medium'
-                  onClick={() => handleClickSymbolName(symbolName)}
-                  type='button'
-                >
+                <button key={symbolName} onClick={() => handleClickSymbolName(symbolName)} type='button'>
                   {symbolName}
                 </button>
               ))}
